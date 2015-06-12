3bc9a8c6946b0b8f6501cf826c8cc05339da2b85
Muhammad Rifqi Priyo Susanto <muhammadrifqipriyosusanto@gmail.com>
2015-06-13 00:58:13 +0700

    Add global variable shout
    Add payment parameter

--- README.md
+++ README.md
@@ -10,11 +10,12 @@ CC0 1.0 Universal
 - (none)
 
 ##Feature
-- Limit who can hear you chat message by distance given in DISTANCE parameter
-- A chat command `/sh` to broadcast your message (pay 1 stick)
+- Limit who can hear you chat message by distance given in shout.DISTANCE parameter
+- A chat command `/sh` to broadcast your message (pay 1 stick by default)
 
 ##Usage
 - Open `init.lua` file by using text editor (e.g. Notepad++, gedit, WordPad, OpenOffice Writer)
-- Edit `DISTANCE` parameter to be distance of chat limit (default: 50)
+- Edit `shout.DISTANCE` parameter to be distance of chat limit (default: 50)
+- Edit `shout.PAYMENT` parameter to be what will player pay to do broadcast (default: "default:stick" 1 stick)
 - Save and run!
 - Note that it doesn't save per world
--- init.lua
+++ init.lua
@@ -1,16 +1,23 @@
 -- Limit chat by distance [shout]
 -- by Muhammad Rifqi Priyo Susanto (srifqi)
 -- License: CC0 1.0 Universal
--- Dependencies: default
+-- Dependencies: (none)
+
+shout = {}
 
 -- Parameter
-local DISTANCE = 50	-- Distance (nodes)
+shout.DISTANCE	= 50	-- Distance (nodes)
+shout.PAYMENT	= {		-- Payment
+	string	= "default:stick",	-- Payment ItemString
+	amount	= 1,
+	name	= "stick",	-- "You don't have any %s to pay."
+} 
 
 --------------------
 -- Internal Stuff --
 --------------------
 
--- Limit chat by distance given in DISTANCE parameter
+-- Limit chat by distance given in shout.DISTANCE parameter
 minetest.register_on_chat_message(function(name, message)
 	local shouter = minetest.get_player_by_name(name)
 	local spos = shouter:getpos()
@@ -22,7 +29,7 @@ minetest.register_on_chat_message(function(name, message)
 	for _,player in ipairs(minetest.get_connected_players()) do
 		if player:get_player_name() ~= shouter:get_player_name() then
 			local pos = player:getpos()
-			if vdistance(spos,pos) <= DISTANCE then
+			if vdistance(spos,pos) <= shout.DISTANCE then
 				minetest.chat_send_player(player:get_player_name(), "<"..name.."> "..message)
 				heard = heard +1
 			end
@@ -35,18 +42,19 @@ end)
 -- Broadcast your message to all player (pay 1 stick)
 minetest.register_chatcommand("sh", {
 	params = "<messsage>",
-	description = "Broadcast your message to all player (pay 1 stick)",
+	description = "Broadcast your message to all player (pay "..shout.PAYMENT.amount.." "..shout.PAYMENT.name..")",
 	privs = {shout=true},
 	func = function(name, param)
 		local player = minetest.get_player_by_name(name)
 		local inv = player:get_inventory()
-		if inv:contains_item("main",ItemStack("default:stick 1")) then
+		local stackname = shout.PAYMENT.string.." "..shout.PAYMENT.amount
+		if inv:contains_item("main",ItemStack(stackname)) then
 			-- Pay 1 stick
-			inv:remove_item("main",ItemStack("default:stick 1"))
+			inv:remove_item("main",ItemStack(stackname))
 			minetest.chat_send_all("<"..name.."> "..param)
 			return true
 		else
-			return false, "You don't have any stick to pay."
+			return false, "You don't have any "..shout.PAYMENT.name.." to pay."
 		end
 	end,
 })
